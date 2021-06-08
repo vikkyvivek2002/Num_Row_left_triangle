927b808c1398981c0339bdfe56403dcf0b408f91
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-08 20:07:06 +0530

    Add files via upload

---
+++ Num_Row_left_triangle.java
@@ -0,0 +1,14 @@
+class Num_Row_left_triangle
+{
+public static void main(String[] args)
+{
+ for(int i=1; i<=5; i++)
+{
+  for(int j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+}
+}
